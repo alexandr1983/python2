12c1b2a46e82369d5bcf430c0235dc1020157037
alexandr1983 <alexandr.mullin@gmail.com>
2021-11-16 11:21:28 +0300

    2.0.1

--- 2.py
+++ 2.py
@@ -1,4 +1,5 @@
 #1
+
 a = [35, 'list', 77, True, None, 45.3, '33',
      (3 + 4j), (5, 43, 6), {44, 55, 33, 8}, frozenset(),
      {"k1": 500, 2: 400, "k2": True, 4: None}, bytearray(b'some text')]
@@ -6,5 +7,12 @@ a = [35, 'list', 77, True, None, 45.3, '33',
 for ind, i in enumerate(a, 1):
     print(f"{ind} {i} {type(i)}")
 
+
 #2
-a = 
+
+a = list(input("Enter numbers:"))
+
+for i in range(1, len(a), 2)
+     a[i - 1], a[i] = a[i], a[i - 1]
+     
+print(a)
